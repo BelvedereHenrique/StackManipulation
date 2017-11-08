1acac0231eb639f7f9eb8e3acb88de6339f8c5ce
Bruno Belvedere <bruno.belvedere@actminds.com>
2017-11-08 08:38:03 -0200

    Initial

---
+++ Stack/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static Stack myStack = new Stack();
+
+        static void Main(string[] args)
+        {
+            ShowMenu();
+
+            while (true)
+            {
+                var keyPressed = Console.ReadKey();
+
+                switch (keyPressed.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        ShowStack();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        PushOnStack();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        PopFromStack();
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.Clear();
+                        ShowMenu();
+                        break;
+                    case ConsoleKey.Enter:
+                        GetMin();
+                        break;
+                    default:
+                        Console.Clear();
+                        ShowMenu();
+                        break;
+                }
+            }
+        }
+
+        private static void GetMin()
+        {
+            Console.WriteLine("");
+            if (myStack.Min() == -1)
+                Console.WriteLine($"Min: Empty.");
+            else
+                Console.WriteLine($"Min: { myStack.Min()}");
+        }
+
+        private static void PushOnStack()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Type an integer number:");
+            int toPush;
+            try
+            {
+                toPush = int.Parse(Console.ReadLine());
+                myStack.Push(toPush);
+                ShowStack();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You need to type an integer number. Operation canceled.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"There was an error while pushing: {e.Message}");
+                Console.WriteLine("Operation canceled.");
+            }
+        }
+
+        private static void ShowMenu()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(" -------------------------------------------------- ");
+            Console.WriteLine("| Press \"Up Arrow\" to Push.                        |");
+            Console.WriteLine("| Press \"Down Arrow\" to pop.                       | ");
+            Console.WriteLine("| Press \"Left Arrow\" to visualize the stack.       | ");
+            Console.WriteLine("| Press \"Right Arrow\" to clear console.            |");
+            Console.WriteLine("| Press \"Enter Key\" to get the minimum stack value.|");
+            Console.WriteLine(" --------------------------------------------------");
+
+        }
+
+        private static void PopFromStack()
+        {
+            try
+            {
+                Console.WriteLine("");
+                Console.WriteLine($"{myStack.Pop()} got popped.");
+                ShowStack();
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Stack is empty.");
+            }
+        }
+
+        private static void ShowStack()
+        {
+            int[] stack = myStack.GetFullStack();
+            Console.WriteLine("");
+            if (stack.Length > 0)
+            {
+                Console.WriteLine("Stack:");
+                for (int i = stack.Length - 1; i >= 0; i--)
+                {
+                    Console.WriteLine($"|{stack[i]}|");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Stack: Empty.");
+            }
+
+
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private int[] _myStack;
+        private int _nextFreeIndex;
+        private int[] _minStack;
+        private int _lastMinIndex;
+        
+        public Stack()
+        {
+            _myStack = new int[0];
+            _minStack = new int[0];
+            _nextFreeIndex = 0;
+            _lastMinIndex = 0;
+        }
+
+        //O(1)
+        public void Push(int value)
+        {
+
+            try
+            {
+                if (_nextFreeIndex == _myStack.Length)
+                    Array.Resize(ref _myStack, _myStack.Length + 1);
+
+                if (_nextFreeIndex == 0)
+                {
+                    Array.Resize(ref _minStack, 1);
+                    _minStack[0] = value;
+                    _myStack[_nextFreeIndex++] = value;
+                    return;
+                }
+
+                _myStack[_nextFreeIndex++] = value;
+
+                if (_myStack[_nextFreeIndex - 1] <= _minStack[_lastMinIndex])
+                {
+                    _lastMinIndex++;
+
+                    if (_lastMinIndex == _minStack.Length)
+                        Array.Resize(ref _minStack, _minStack.Length + 1);
+
+                    _minStack[_lastMinIndex] = value;
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new IndexOutOfRangeException("The stack is full.");
+            }
+        }
+
+        //O(1)
+        public int Pop()
+        {
+            if (_nextFreeIndex == 0)
+                throw new InvalidOperationException("Empty stack.");
+
+            _nextFreeIndex--;
+
+            int returnValue = _myStack[_nextFreeIndex];
+
+            if (returnValue == _minStack[_lastMinIndex])
+            {
+                if (_lastMinIndex == 0)
+                {
+                    Array.Resize(ref _minStack, 0);
+                    _lastMinIndex = 0;
+                }
+                else
+                {
+                    Array.Resize(ref _minStack, _minStack.Length - 1);
+                    _lastMinIndex--;
+                }
+
+            }
+
+            if (_myStack.Length == 1)
+                Array.Resize(ref _myStack, 0);
+            else if (_myStack.Length > 1)
+                Array.Resize(ref _myStack, _myStack.Length - 1);
+
+            return returnValue;
+        }
+
+        //O(1)
+        public int Min()
+        {
+            try
+            {
+                return _minStack[_lastMinIndex];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return -1;
+            }
+        }
+
+        public int[] GetFullStack()
+        {
+            return _myStack;
+        }
+    }
+}
